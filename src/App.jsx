1f82120b40b01478d9c48218a8efdd802d2cecba
Ruchiket Borse <RuchiketBorse1@gmail.com>
2024-01-22 08:53:54 +0000

    created exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,26 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import * as React from 'react'
 
-function App() {
-	const [count, setCount] = useState(0);
+function Hello() {
+  // delete this variable declaration and replace it with a React.useState call
+  const name = ''
 
-	return (
-		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noreferrer">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
-	);
+  function handleChange(event) {
+    // update the name here based on event.target.value
+  }
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
 }
 
-export default App;
+function App() {
+  return <Hello />
+}
+
+export default App
